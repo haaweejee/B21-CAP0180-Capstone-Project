3dded39571219b0dbc59cf53731c898827ed7f65
hendrawanwib <hendrawanwib@gmail.com>
2021-05-26 13:07:10 +0700

    fix layout

---
+++ app/src/main/java/id/bangkit2021/capstoneproject/ui/PreferenceActivity.kt
@@ -0,0 +1,17 @@
+package id.bangkit2021.capstoneproject.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.bangkit2021.capstoneproject.R
+
+class PreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preference)
+
+
+    }
+
+
+
+}
