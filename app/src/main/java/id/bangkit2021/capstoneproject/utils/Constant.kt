a205bf3cc203ddb4eb37312ec8596683d08209f1
hendrawanwib <hendrawanwib@gmail.com>
2021-05-26 21:49:42 +0700

    change tflite model

--- app/src/main/java/id/bangkit2021/capstoneproject/utils/Constant.kt
+++ app/src/main/java/id/bangkit2021/capstoneproject/utils/Constant.kt
@@ -4,8 +4,8 @@ class Constant {
     companion object{
         const val GET_IMAGE_FILE = 100
         const val GET_IMAGE_FROM_CAMERA = 110
-        const val TFLITE_FILE = "converted_model.tflite"
+        const val TFLITE_FILE = "tflite_model.tflite"
         const val TFLITE_LABEL = "labels.txt"
-        const val TFLITE_INPUT_SIZE = 224
+        const val TFLITE_INPUT_SIZE = 150
     }
 }
